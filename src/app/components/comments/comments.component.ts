4c231562456c17e48cea1a6f102a8a64102d8e40
Martin Gutierrez <pablo.martingutierrez.external@atos.net>
2018-11-22 12:44:38 +0000

    Async comments in Comments-component. add Comments to Post-component

--- src/app/components/comments/comments.component.ts
+++ src/app/components/comments/comments.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { JsonService } from 'src/app/services/json.service';
-import { Observable, zip } from 'rxjs';
+import { Observable, zip, of } from 'rxjs';
+import { map, mergeMap, tap, groupBy } from 'rxjs/operators';
 import { Comment } from 'src/app/models/comment';
 import { Post } from 'src/app/models/post';
 
@@ -11,34 +12,26 @@ import { Post } from 'src/app/models/post';
 })
 export class CommentsComponent implements OnInit {
 
-  public comments$: Observable<Comment[]>;
   public posts$: Observable<Post[]>;
-
-  public commentsP: Promise<Comment[]>;
-  public postsP: Promise<Post[]>;
-
-  public posts: Post[];
-  public comments: Comment[];
 
   constructor(private service: JsonService) {}
 
   ngOnInit() {
+    this.posts$ = this
+      .awaitPostsAndComments()
+      .pipe(map(this.addCommentsToPosts()));
+  }
 
-    this.posts$ = this.service.getPosts();
-    this.comments$ = this.service.getComments();
+  private awaitPostsAndComments() {
+    return zip(this.service.getPosts(), this.service.getComments());
+  }
 
-    // En pares. Se emite valor cuando llegan al mismo ¿indice?
-    zip(this.posts$, this.comments$)
-      .subscribe(([posts, comments]) => {
-        this.posts = posts.map(post => {
-          return post = { ...post, comments: comments.filter(comment => comment.postId === post.id) };
-        });
+  private addCommentsToPosts(): (value: [Post[], Comment[]]) => Post[] {
+    return ([posts, comments]) => {
+      posts = posts.map(post => {
+        return { ...post, comments: comments.filter(comment => post.id === comment.postId) };
       });
-
-    // Se emite valor cuando se termina todo
-    // Si es a mano, llamar a .complete() (no en este caso)
-    // forkJoin(this.posts$, this.comments$)
-    // Se emite valor con cualquier cambio
-    // combineLatest(this.posts$, this.comments$)
+      return posts;
+    };
   }
 }
--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -2,10 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { JsonService } from 'src/app/services/json.service';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
-import { concat } from 'rxjs/operators';
+import { mergeMap } from 'rxjs/operators';
 import { Post } from 'src/app/models/post';
 import { Comment } from 'src/app/models/comment';
-import { async } from 'rxjs/internal/scheduler/async';
 
 @Component({
   selector: 'app-post',
@@ -20,24 +19,18 @@ export class PostComponent implements OnInit {
   constructor(private service: JsonService, private route: ActivatedRoute) { }
 
   ngOnInit() {
-    const id = this.route.snapshot.params['id'];
-    this.post$ = this.service.getPostBy(id);
-    // this.comments$ = this.service.getCommentsByPost(this.post$ | async);
-    
-    
-    const both = this.post$
-      .pipe(concat(this.comments$))
-      .subscribe((postAndComments) => {
-        const post = postAndComments[0];
-        const comments = postAndComments[1];
-
-        console.log({post});
-        console.log({comments});
-      });
-
-    this.post$.subscribe(post => {
-      this.comments$ = this.service.getCommentsByPost(post.id);
-    });
+    this.getPost();
+    this.getPostComments();
   }
 
+  private getPost() {
+    const id = this.route.snapshot.params['id'];
+    this.post$ = this.service.getPostBy(id);
+  }
+  
+  private getPostComments() {
+    this.comments$ = this.post$.pipe(mergeMap(post => {
+      return this.service.getCommentsByPost(post.id);
+    }));
+  }
 }
