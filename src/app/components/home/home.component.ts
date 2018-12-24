a2362423f2aba1c7f604729c667f75b2176124fc
Pablo <pablo.martingu@gmail.com>
2018-12-24 19:44:49 +0000

    [textContent] existe!

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,7 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  constructor() {}
 
-  ngOnInit() {}
+  private _text = 'Texto inicial del span';
+  private frases = [
+    'Texto random 1',
+    'Texto random 2',
+    'Texto random 3',
+    'Texto random 4',
+    'Texto random 5',
+  ];
+
+  ngOnInit() {
+    this.setRandomTextEvery(1000);
+  }
+
+  /**
+   * Sets an interval to change the value of _time
+   * @param time ms
+   */
+  private setRandomTextEvery(time: number) {
+    setInterval(this.setRandomText.bind(this), time);
+  }
+
+  private setRandomText() {
+    const index = Math.floor(Math.random() * 5);
+    return this._text = this.frases[index];
+  }
+
+  get text() {
+    return this._text;
+  }
 }
