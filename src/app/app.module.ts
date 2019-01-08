515a196b0387126fd59227d66f04a242df54268d
Martin Gutierrez <pablo.martingutierrez.external@atos.net>
2019-01-08 14:24:52 +0000

    move post to ornaginsms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,6 @@ import { CapitalizePipe } from './pipes/capitalize.pipe';
 import { AppComponent } from './app.component';
 import { UserComponent } from './components/user/user.component';
 import { PhotoComponent } from './components/photo/photo.component';
-import { PostComponent } from './components/post/post.component';
 import { AlbumComponent } from './components/album/album.component';
 import { HomeComponent } from './components/layouts/home/home.component';
 import { CommentsComponent } from './components/layouts/comments/comments.component';
@@ -24,11 +23,12 @@ import { AudioComponent } from './components/layouts/audio/audio.component';
 import { PhotosComponent } from './components/layouts/photos/photos.component';
 import { PostsComponent } from './components/layouts/posts/posts.component';
 import { PhotoMiniatureComponent } from './components/layouts/photos/photo-miniature/photo-miniature.component';
-import { PostMiniatureComponent } from './components/layouts/posts/post-miniature/post-miniature.component';
 import { UsersComponent } from './components/layouts/users/users.component';
 import { AlbumsComponent } from './components/layouts/albums/albums.component';
 import { AlbumMiniatureComponent } from './components/layouts/albums/album-miniature/album-miniature.component';
 import { TableComponent } from './components/organisms/table/table.component';
+import { PostMiniatureComponent } from './components/organisms/post-miniature/post-miniature.component';
+import { PostComponent } from './components/layouts/post/post.component';
 
 @NgModule({
   declarations: [
